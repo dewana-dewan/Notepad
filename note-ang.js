493e2241e05bf4f0cf065d92698b695974dd5696
Dewan <iit2015097@iiita.ac.in>
2016-11-25 18:48:56 +0530

    previous commits angular related data changed

---
+++ note-ang.js
@@ -0,0 +1,17 @@
+(function() {
+
+  'use strict';
+
+  /**
+   * angular
+   * Description: Angular!
+   */
+  angular
+
+  /**
+   * app
+   * Description: Sets up the initial state of the application
+   */
+  .module('app', [])
+
+})();
